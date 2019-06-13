2f9415f2d4598936542ed2adea05e104b024f6ce
CMHayden <haydencallum4@gmail.com>
2019-06-13 12:02:18 +0200

    create post to sales route

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 Route::post('products', 'ProductsController@store');
+
+Route::post('sales', 'SalesController@store');
