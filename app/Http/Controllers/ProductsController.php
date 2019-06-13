484356b5657d0b4f7c7b185f053d8fa08ad01856
CMHayden <haydencallum4@gmail.com>
2019-06-13 15:41:33 +0200

    overwrite handle notification function in products controller

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -3,8 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Log;
 
-class ProductsController extends Controller
+class ProductsController extends SNSController
 {
-    //
+    public function handleNotification($notification) {
+
+        $guzzleClient = new \GuzzleHttp\Client();
+
+        $guzzleClient->post("https://en8bgh682bive.x.pipedream.net/", ['body' => json_encode($notification)]);
+
+        return app('Illuminate\Http\Response')->status();
+
+    }
 }
