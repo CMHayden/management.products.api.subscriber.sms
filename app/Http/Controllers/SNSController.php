1e393eb640670e96683849c75825e8ec7b6177fd
CMHayden <haydencallum4@gmail.com>
2019-06-13 15:40:41 +0200

    create functions for handling request, handling notification and subscribing

--- app/Http/Controllers/SNSController.php
+++ app/Http/Controllers/SNSController.php
@@ -3,8 +3,38 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Log;
 
 class SNSController extends Controller
 {
-    //
+    public function handleRequest(Request $request) {
+
+        Log::info('Inside handleRequest');
+        $requestJSON =  json_decode($request->getContent(), true);
+
+        if ($requestJSON['Type']  == 'SubscriptionConfirmation') {
+            return $this->subscribe($requestJSON);
+
+        }
+
+        $this->handleNotification($requestJSON);
+    }
+
+    private function subscribe($notification) {
+
+        Log::info("Subscribing to topic");
+
+        $guzzleClient = new \GuzzleHttp\Client();
+
+        $guzzleResponse = $guzzleClient->get($notification['SubscribeURL']);
+
+        Log::info('Successfully subscribed');
+
+        return app('Illuminate\Http\Response')->status();
+
+    }
+
+    protected function handleNotification($notification) {
+
+    }
 }
