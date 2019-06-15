080c31763e911fcc695df8ed5d37fd212ed779a6
CMHayden <haydencallum4@gmail.com>
2019-06-15 22:20:19 +0200

    modify products controller for logic for sending sms through twilio

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -3,15 +3,37 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Log;
+use Twilio\Rest\Client;
 
 class ProductsController extends SNSController
 {
-    public function handleNotification($notification) {
+    public function handleRequest(Request $request, $phone_number) {
 
-        $guzzleClient = new \GuzzleHttp\Client();
+        $requestJSON =  json_decode($request->getContent(), true);
 
-        $guzzleClient->post("https://en8bgh682bive.x.pipedream.net/", ['body' => json_encode($notification)]);
+        if ($requestJSON['Type']  == 'SubscriptionConfirmation') {
+
+            return $this->subscribe($requestJSON);
+
+        }
+
+        $text_message = $requestJSON['Message'];
+
+        $this->sendSMS($phone_number, $text_message);
+    }
+
+    public function sendSMS($number, $text_message) {
+
+        $sid        = "AC234d8310cc34877c447963f0529f89b4";
+        $token      = "285254bcbc39faf41cf6722f698281df";
+        $twilio     = new Client($sid, $token);
+        $message_body = json_encode($text_message);
+
+        $message = $twilio  ->messages
+                            ->create("$number",
+                                    array("from" => "+441603340331",
+                                            "body" => "$message_body")
+                                    );
 
         return app('Illuminate\Http\Response')->status();
 
